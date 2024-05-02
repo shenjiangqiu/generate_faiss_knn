baa98657f19bb45233a8ca171f9e083fc99fd927
Jiangqiu Shen <ivy22233qiu@live.com>
2024-05-02 19:27:08 -0400

    update tasks

---
+++ generate_sift.sh
@@ -0,0 +1,3 @@
+cargo run --bin gtrue --release -- ./dataset/deep/deep_100M_base.fvecs ./dataset/deep/deep_100M_query_100K.fvecs 100 ./dataset/deep/deep_100M_gt.ivecs
+cargo run --bin gtrue --release -- dataset/sift_100M/sift_100M.fvecs dataset/sift_100M/sift_100M_query_100K.fvecs 100 dataset/sift_100M/sift_100M_gt.fvecs.ivecs
+cargo run --bin gtrue --release -- dataset/spacev/spacev100m_base.fvecs dataset/spacev/spaceev_100M_query_100K.fvecs 100 dataset/spacev/spaceev_100M_gt.ivecs
